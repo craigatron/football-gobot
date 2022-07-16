cdf2b2bf5abc0db8abda52b282be5783e5ad52fa
Craig Martek <craigmartek@gmail.com>
2022-07-16 00:04:19 -0400

    lint

--- config.go
+++ config.go
@@ -10,10 +10,13 @@ import (
 	"cloud.google.com/go/storage"
 )
 
+// LeagueType is the type of league represented by a particular Discord category.
 type LeagueType int32
 
 const (
+	// LeagueTypeESPN is an ESPN fantasy football league.
 	LeagueTypeESPN LeagueType = iota
+	// LeagueTypeSleeper is a Sleeper fantasy football league.
 	LeagueTypeSleeper
 )
 
