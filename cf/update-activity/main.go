ffda162703dcccadb8ed3cfd3511488471581c4e
Craig Martek <craigmartek@gmail.com>
2022-07-18 20:30:24 -0400

    lint

--- cf/update-activity/main.go
+++ cf/update-activity/main.go
@@ -4,15 +4,18 @@ import (
 	"context"
 	"log"
 
+	// Need this for cloud functions
 	_ "github.com/GoogleCloudPlatform/functions-framework-go/funcframework"
 
 	"github.com/craigatron/football-gobot/config"
 )
 
+// PubsubMessage is the type of the message triggering this function.
 type PubsubMessage struct {
 	Data []byte `json:"data"`
 }
 
+// UpdateActivity is the entry point for this cloud function.
 func UpdateActivity(ctx context.Context, m PubsubMessage) error {
 	conf, err := config.LoadConfig()
 	if err != nil {
